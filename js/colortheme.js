cc7b3f3ae4283270e67dc95a44b9cf3ad6cf5d30
snipchu <pipandw@gmail.com>
2024-03-20 22:40:45 -0500

    the color palette loads automatically! thank you localstorage!

---
+++ js/colortheme.js
@@ -0,0 +1,14 @@
+function togglecolor() {
+  document.querySelector(':root').toggleAttribute('darkmode');
+  var darkon = document.documentElement.getAttribute("darkmode")=="";
+  window.localStorage.setItem("darkmode", darkon); 
+}
+
+function onload() {
+  if (localStorage.getItem("darkmode")=="true") {
+    document.querySelector(':root').setAttribute('darkmode', "");
+  } else {
+    document.querySelector(':root').removeAttribute('darkmode');
+  }
+}
+onload();
