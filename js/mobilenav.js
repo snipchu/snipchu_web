ac45e3a5c0b69ce72017e077e71b768ad414f52a
snipchu <pipandw@gmail.com>
2024-04-03 20:39:01 -0500

    dark mode icon, user preferences can change theme color, and some more awesome stuff!

--- js/mobilenav.js
+++ js/mobilenav.js
@@ -1,13 +1,14 @@
 var nav = document.getElementById("menuitems");
 var hammenu = document.getElementById("hamicon");
 function showmenu() {
-  if (nav.style.display == "flex") {
-    nav.style.display = "";
+  if (nav.lastElementChild.style.display == "block") {
+    nav.lastElementChild.style.display = "";
     hammenu.classList.remove("nf-md-menu_open");
     hammenu.classList.add("nf-md-menu");
   } else {
-    nav.style.display = "flex";
+    nav.lastElementChild.style.display = "block";
     hammenu.classList.remove("nf-md-menu");
     hammenu.classList.add("nf-md-menu_open");
   }
 }
+
