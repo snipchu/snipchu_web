998b69a97f0d8408349d029ca66dc3593d015fbd
snipchu <pipandw@gmail.com>
2024-04-05 20:57:15 -0500

    statuscafe improvements and arialabels

--- js/statuscafe.js
+++ js/statuscafe.js
@@ -1,6 +1,5 @@
 fetch("https://status.cafe/users/snipchu/status.json")
   .then( r => r.json() )
   .then( r => {
-    document.getElementById("statuser").innerHTML = r.face + ' <a href="https://status.cafe/users/snipchu">' + r.timeAgo + "</a>"
-    document.getElementById("statcont").innerHTML = r.content
+    document.getElementById("statuscafe").innerHTML = r.face + ' <a href="https://status.cafe/users/snipchu">' + r.timeAgo + '</a><hr>' + r.content
   })
